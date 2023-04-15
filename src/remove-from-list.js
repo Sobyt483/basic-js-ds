c62945c1a4cfca1ba9413d40f616bdc92df9715c
Sobyt <kravczov.andrian@mail.ru>
2023-04-15 21:21:46 +0300

    feat: finish task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  function arrayFromList(list){
+    const array = []
+    while(list!== null){
+      array.push(list.value)
+      list = list.next
+    }
+    return array
+  }
+  const array = arrayFromList(l)
+  const resArray = array.filter((el)=>el!==k)
+  function convertArrayToList(arr) {
+    return arr.reverse().reduce((acc, cur) => {
+      if (acc) {
+        const node = new ListNode(cur);
+        node.next = acc;
+        return node;
+      }
+      return new ListNode(cur);
+    }, null);
+  }
+  return convertArrayToList(resArray)
 }
-
 module.exports = {
   removeKFromList
 };
